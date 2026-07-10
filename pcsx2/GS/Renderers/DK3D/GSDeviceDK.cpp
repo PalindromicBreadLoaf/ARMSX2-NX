2ff2c413c5b97091335394d98b717f64f10a1a38
PalindromicBreadLoaf <palindromicbreadloaf@tuta.com>
2026-07-10 11:00:45 -0400

    Whitespace cleanup

--- pcsx2/GS/Renderers/DK3D/GSDeviceDK.cpp
+++ pcsx2/GS/Renderers/DK3D/GSDeviceDK.cpp
@@ -990,7 +990,7 @@ void GSDeviceDK::RenderImGui()
 	const ImDrawData* draw_data = ImGui::GetDrawData();
 	if (!draw_data || draw_data->CmdListsCount == 0 || !m_imgui_shaders_ok || !m_frame_active || m_present_slot < 0)
 		return;
-	
+
 	UpdateImGuiTextures();
 
 	InvalidateHWStateCache();
